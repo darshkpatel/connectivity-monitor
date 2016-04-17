c03d33c42d83add2937ba6e7be9893e1dc423dc0
Darsh Patel <darshkpatel@users.noreply.github.com>
2016-04-17 20:30:46 +0530

    Added files via upload

---
+++ connectivity-monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true;
+do
+  ping -c1 -W2 216.58.199.174 > /dev/null
+  if [ "$?" = 0 ]
+  then
+    sleep 30
+  else
+   NOW=$(date +%H:%M:%S\ %d-%m-%y)
+   wall "Internet is Down ! - $NOW"
+   sleep 60
+  fi
+done
